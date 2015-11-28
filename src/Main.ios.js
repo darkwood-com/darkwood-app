c78f97a28787af8886335ded5de52d6cde12d222
Mathieu Ledru <matyo91@gmail.com>
2015-11-28 21:04:14 +0100

    header

--- src/Main.ios.js
+++ src/Main.ios.js
@@ -4,6 +4,7 @@ var I18n = require('react-native-i18n');
 var {
   StyleSheet,
   Text,
+  Image,
   View,
   TabBarIOS,
   StatusBarIOS
@@ -22,52 +23,69 @@ var Main = React.createClass({
   },
   render: function() {
     return (
-      <TabBarIOS>
-        <TabBarIOS.Item
-          title={ I18n.t("tap_play") }
-          icon={ require('image!play') }
-          onPress={ () => this.changeTab('play') }
-          selected={ this.state.selectedTab === 'play' }>
-          <View style={ styles.pageView }>
-            <Text>Play</Text>
-          </View>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title={ I18n.t("tap_say") }
-          icon={ require('image!say') }
-          onPress={ () => this.changeTab('say') }
-          selected={ this.state.selectedTab === 'say' }>
-          <View style={ styles.pageView }>
-            <Text>Say</Text>
-          </View>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title={ I18n.t("tap_users") }
-          icon={ require('image!users') }
-          onPress={ () => this.changeTab('users') }
-          selected={ this.state.selectedTab === 'users' }>
-          <View style={ styles.pageView }>
-            <Text>Users</Text>
-          </View>
-        </TabBarIOS.Item>
-        <TabBarIOS.Item
-          title={ I18n.t("tap_account") }
-          icon={ require('image!account') }
-          onPress={ () => this.changeTab('account') }
-          selected={ this.state.selectedTab === 'account' }>
-          <View style={ styles.pageView }>
-            <Text>Account</Text>
-          </View>
-        </TabBarIOS.Item>
-      </TabBarIOS>
+      <View style={ styles.pageView }>
+        <View style={ styles.headerView }>
+          <Image
+            style={ styles.image }
+            source={ require('image!header') }
+            />
+        </View>
+        <TabBarIOS style={ styles.pageView }>
+          <TabBarIOS.Item
+            title={ I18n.t("tap_play") }
+            icon={ require('image!play') }
+            onPress={ () => this.changeTab('play') }
+            selected={ this.state.selectedTab === 'play' }>
+            <View style={ styles.pageView }>
+              <Text>Play</Text>
+            </View>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title={ I18n.t("tap_say") }
+            icon={ require('image!say') }
+            onPress={ () => this.changeTab('say') }
+            selected={ this.state.selectedTab === 'say' }>
+            <View style={ styles.pageView }>
+              <Text>Say</Text>
+            </View>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title={ I18n.t("tap_users") }
+            icon={ require('image!users') }
+            onPress={ () => this.changeTab('users') }
+            selected={ this.state.selectedTab === 'users' }>
+            <View style={ styles.pageView }>
+              <Text>Users</Text>
+            </View>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item
+            title={ I18n.t("tap_account") }
+            icon={ require('image!account') }
+            onPress={ () => this.changeTab('account') }
+            selected={ this.state.selectedTab === 'account' }>
+            <View style={ styles.pageView }>
+              <Text>Account</Text>
+            </View>
+          </TabBarIOS.Item>
+        </TabBarIOS>
+      </View>
     );
   }
 });
 
 var styles = StyleSheet.create({
   pageView: {
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     flex: 1
+  },
+  headerView: {
+    flexDirection: 'column',
+    height: 100,
+    alignItems: 'center'
+  },
+  image: {
+    flex:1,
+    resizeMode: 'contain'
   }
 });
 
