f5c46e82ab5f3bf09ad110111a36162b6e377a16
Mathieu Ledru <matyo91@gmail.com>
2015-12-05 02:28:43 +0100

    build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-react-native bundle --minify --bundle-output ios/main.jsbundle --entry-file index.ios.js --dev false --platform ios
+react-native bundle --minify --bundle-output ios/main.jsbundle --entry-file index.ios.js --dev false --platform ios --assets-dest ios
