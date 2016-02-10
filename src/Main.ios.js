599c4c0983dd95a36eeadc0f74c9d6ebe2a0f46e
Mathieu Ledru <matyo91@gmail.com>
2016-02-10 15:47:49 +0100

    images

--- src/Main.ios.js
+++ src/Main.ios.js
@@ -33,34 +33,34 @@ var Main = React.createClass({
         <View style={ styles.headerView }>
           <Image
             style={ styles.image }
-            source={ require('image!header') }
+            source={ require('./img/header.png') }
             />
         </View>
         <TabBarIOS style={ styles.pageView }>
           <TabBarIOS.Item
             title={ I18n.t("tap_play") }
-            icon={ require('image!play') }
+            icon={ require('./img/play.png') }
             onPress={ () => this.changeTab('play') }
             selected={ this.state.selectedTab === 'play' }>
             <Pages ref='play' route="play" />
           </TabBarIOS.Item>
           <TabBarIOS.Item
             title={ I18n.t("tap_chat") }
-            icon={ require('image!say') }
+            icon={ require('./img/say.png') }
             onPress={ () => this.changeTab('chat') }
             selected={ this.state.selectedTab === 'chat' }>
             <Pages ref='chat' route="chat" />
           </TabBarIOS.Item>
           <TabBarIOS.Item
             title={ I18n.t("tap_users") }
-            icon={ require('image!users') }
+            icon={ require('./img/users.png') }
             onPress={ () => this.changeTab('users') }
             selected={ this.state.selectedTab === 'users' }>
             <Pages ref='users' route="users" />
           </TabBarIOS.Item>
           <TabBarIOS.Item
             title={ I18n.t("tap_account") }
-            icon={ require('image!account') }
+            icon={ require('./img/account.png') }
             onPress={ () => this.changeTab('account') }
             selected={ this.state.selectedTab === 'account' }>
             <Pages ref='account' route="account" />
@@ -84,6 +84,10 @@ var styles = StyleSheet.create({
   image: {
     flex:1,
     resizeMode: 'contain'
+  },
+  tabBarItem: {
+    width: 20,
+    height: 20
   }
 });
 
